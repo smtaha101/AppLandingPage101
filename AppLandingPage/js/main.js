fc4435c31f4815625a758ef7d83e5154eeb910ee
smtaha101 <73366193+smtaha101@users.noreply.github.com>
2021-08-16 10:02:57 -0700

    Add files via upload

---
+++ AppLandingPage/js/main.js
@@ -0,0 +1,86 @@
+
+
+// ******************* Feature Carousel Start ********************
+$(".feature-carousel").owlCarousel(
+{
+    loop:true,
+    margin:0,
+    responsiveClass:true,
+    responsive:{
+        0:{
+            items:1,
+        },
+        600:{
+            items:2,
+        },
+        1000:{
+            items:3,
+        }
+    }
+});
+// ******************* Feature Carousel End ********************
+
+
+
+// ******************* Screenshot Carousel Start ********************
+$(".screenshot-carousel").owlCarousel(
+    {
+        loop:true,
+        margin:0,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:2,
+            },
+            1000:{
+                items:4,
+            }
+        }
+    });
+// ******************* Screenshot Carousel End ********************
+
+
+
+// ******************** Testimonials Carousel Start ******************
+$(".testimonialcarousel").owlCarousel(
+    {
+        loop:true,
+        margin:0,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:2,
+            },
+            1000:{
+                items:3,
+            }
+        }
+    });
+// ******************** Testimonials Carousel End ******************
+
+
+// ********************* Team Carousel Start ************************
+$(".teamcarousel").owlCarousel(
+    {
+        loop:true,
+        margin:0,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:2,
+            },
+            1000:{
+                items:3,
+            }
+        }
+    });
+// ********************* Team Carousel End ************************
